c1a17a8c1e5357606d1c36f130390832911c624a
bbbluesky1 <91199395+bbbluesky1@users.noreply.github.com>
2023-06-11 03:07:12 +0900

    Update q_2.py

--- py_file/q_2.py
+++ py_file/q_2.py
@@ -1,13 +1,13 @@
 for i in range(1,testcase[0]+1) :
     
     if testcase[i] % 15 == 0 :
-        print("FizzBuzz")
+        algoPrint("FizzBuzz")
 
     elif testcase[i] % 5 == 0 :
-        print("Buzz")
+        algoPrint("Buzz")
 
     elif testcase[i] % 3 == 0 :
-        print("Fizz")
+        algoPrint("Fizz")
         
     else :
-        print("")
+        algoPrint("")
